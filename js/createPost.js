53f11b92cd43d387f16184d9199fef89ce53ed31
maleneivy <68667843+maleneivy@users.noreply.github.com>
2023-10-06 19:18:46 +0200

    added html for fetching posts in the feed page

--- js/createPost.js
+++ js/createPost.js
@@ -2,22 +2,24 @@ import { API_BASE_URL } from "./settings/baseUrl.mjs";
 import { token } from "./utils/storage.mjs";
 
 const createPostContainer = document.querySelector("#new-post-form");
-const postTitle = createPostContainer.querySelector("#postTitle");
-const postBody = createPostContainer.querySelector("#postBody");
+const formTitle = document.querySelector("#postTitleInput");
+const formBody = document.querySelector("#postBodyInput");
+
+
 
 createPostContainer.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const titleValue = postTitle.value;
-    const postBodyValue = postBody.value;
+    const titleValue = formTitle.value;
+    const postBodyValue = formBody.value;
 
     createPost(titleValue, postBodyValue);
 });
 
-async function createPost(title, body) {
+async function createPost(titleValue, postBodyValue) {
     const createPostsUrl = `${API_BASE_URL}/api/v1/social/posts`;
 
-    const postData = JSON.stringify({ title: title, body: body });
+    const postData = JSON.stringify({ title: titleValue, body: postBodyValue });
 
     const postOptions = {
         method: "POST",
@@ -33,6 +35,8 @@ async function createPost(title, body) {
         const json = await response.json();
         console.log(response)
         console.log(json)
+
+
     } catch (error) {
         console.log(error)
     }
