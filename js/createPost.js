dc2e3a9b95744a2a0067fb75fa9ecc13e3e491c1
maleneivy <68667843+maleneivy@users.noreply.github.com>
2023-10-11 17:49:50 +0200

    displayMessage function and create post message handling

--- js/createPost.js
+++ js/createPost.js
@@ -1,16 +1,19 @@
 import { API_BASE_URL } from "./settings/baseUrl.mjs";
 import { token } from "./utils/storage.mjs";
+import { clearMessages, displayMessage } from "./components/displayMessage.mjs";
+
 
 const createPostContainer = document.querySelector("#new-post-form");
 const formTitle = document.querySelector("#postTitleInput");
 const formBody = document.querySelector("#postBodyInput");
 const formTags = document.querySelector("#postTagsInput");
 const formImage = document.querySelector("#postImageInput");
-
+const messageContainerId = "#message-container";
 
 
 createPostContainer.addEventListener("submit", (e) => {
     e.preventDefault();
+    clearMessages(messageContainerId);
 
     const titleValue = formTitle.value;
     const postBodyValue = formBody.value;
@@ -38,17 +41,27 @@ async function createPost(titleValue, postBodyValue, postTagsValue, postImageVal
     try {
         const response = await fetch(createPostsUrl, postOptions);
         const json = await response.json();
-        console.log(response)
-        console.log(json)
+        const jsonErrors = json.errors;
 
         if (response.ok) {
-            console.log("Success!")
+            displayMessage("success-message", `The post was successfully uploaded`, messageContainerId);
+        } else if (jsonErrors) {
+            clearMessages(messageContainerId);
+            jsonErrors.forEach((errorMessage) => {
+                displayMessage("error-message", errorMessage.message, messageContainerId, true);
+            });
         }
-        else {
-            console.log("Failed to upload new post")
-        }
-
     } catch (error) {
         console.log(error)
+        displayMessage("error-message", "Something went wrong", messageContainerId);
     }
 };
+
+/*
+https://i.postimg.cc/tRV6W8sy/mango-catfish-taco-2944558-1280.jpg
+
+Proin ac vulputate ante, eget elementum nisl. Vestibulum sagittis quam tempus, interdum libero quis, semper mauris. Suspendisse molestie est nec auctor consequat.
+
+Phasellus auctor scelerisque nibh, sit amet dignissim elit rutrum eget. Etiam volutpat laoreet cursus. Mauris tincidunt nunc eu velit euismod, imperdiet vehicula ante luctus. Nunc et vehicula metus. Praesent ut venenatis quam. Phasellus vel sapien nisi. Nullam faucibus sapien eu felis tincidunt, vitae elementum ante euismod. Suspendisse potenti. Proin porttitor quam vitae gravida tristique. Nulla facilisi.
+
+*/
